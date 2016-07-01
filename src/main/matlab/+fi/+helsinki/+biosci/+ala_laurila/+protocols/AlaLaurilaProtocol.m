e3a0edcb7ffc63031ad78dcf111323987a417d6d
ragavsathish <sathish_narayanan@outlook.com>
2016-07-01 14:20:23 +0300

    added read only ndf to protocol parameters

--- src/main/matlab/+fi/+helsinki/+biosci/+ala_laurila/+protocols/AlaLaurilaProtocol.m
+++ src/main/matlab/+fi/+helsinki/+biosci/+ala_laurila/+protocols/AlaLaurilaProtocol.m
@@ -1,6 +1,29 @@
 classdef (Abstract) AlaLaurilaProtocol < symphonyui.core.Protocol
     
+    properties (Dependent, SetAccess = private)
+        amp2    % Secondary amplifier one
+        amp3    % Secondary amplifier two
+        amp4    % Secondary amplifier three
+        ndfs    % Selected ndfs. To change ndf press ctrl+D and change for given LED
+    end
+    
+    properties(Hidden)
+        ampList = {'amp2', 'amp3', 'amp4'};
+    end
+    
     methods
+        
+        function d = getPropertyDescriptor(obj, name)
+            d = getPropertyDescriptor@symphonyui.core.Protocol(obj, name);
+            
+            if ismember(name, obj.ampList) && isempty(obj.getSecondaryAmp(name))
+                d.isHidden = true;
+            end
+            
+            if strncmp(name, 'ndfs', 4) && isempty(obj.ndfs)
+                d.isHidden = true;
+            end
+        end
         
         function prepareEpoch(obj, epoch)
             prepareEpoch@symphonyui.core.Protocol(obj, epoch);
@@ -31,6 +54,40 @@ classdef (Abstract) AlaLaurilaProtocol < symphonyui.core.Protocol
             end
         end
         
+        function amp = get.amp2(obj)
+            amp = obj.getSecondaryAmp('amp2');
+        end
+        
+        function amp = get.amp3(obj)
+            amp = obj.getSecondaryAmp('amp3');
+        end
+        
+        function amp = get.amp4(obj)
+            amp = obj.getSecondaryAmp('amp4');
+        end
+        
+        function amp = getSecondaryAmp(obj, name)
+            ampName = [upper(name(1)) name(2:end)];
+            amp = obj.rig.getDeviceNames(ampName);
+            if ~ isempty(amp)
+                amp = amp{:};
+            end
+        end
+        
+        function ndfs = get.ndfs(obj)
+            ndfs = obj.getSelectedNdfs();
+        end
+        
+        % default configuration for led device.
+        % Can be overridden, if stage is configured for netural density filter
+        function ndfs = getSelectedNdfs(obj)
+            ndfs = [];
+            if ~ isprop(obj, 'led')
+                return;
+            end
+            ledDevice = obj.rig.getDevice(obj.led);
+            ndfs = ledDevice.getConfigurationSetting('ndfs');
+        end
     end
     
 end
